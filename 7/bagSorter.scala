157db3dda3e07232eda7600b5103c2f0cb20d4be
Alex Sikiaridis <alex@alexs-mbp.home>
2020-12-07 22:01:52 +0100

    Adds solution to day 7

---
+++ 7/bagSorter.scala
@@ -0,0 +1,43 @@
+import scala.io.Source
+import scala.collection.immutable.HashMap
+
+object toboganSteer {
+  def main(args: Array[String]): Unit = {
+    val filename: String        = "bagRules.txt"
+    val rules: List[String] = Source.fromFile(filename).getLines.toList
+
+    
+    def addInvertBagMap(rule: String, initialMap: HashMap[String, List[String]]): HashMap[String, List[String]] = {
+      val containedRegex = "\\d+([\\w ])+[bags|bag][, |.]".r
+
+      val outerColour: String = rule.split(" ").slice(0, 2).mkString(" ")
+      val containedColourRules: List[String] = containedRegex.findAllIn(rule).toList
+      containedColourRules.foldLeft(initialMap)((currentMap, containedRule) => {
+        val containedColour: String = containedRule.split(" ").slice(1, 3).mkString(" ")
+        currentMap.get(containedColour) match {
+          case Some(existingColours) if existingColours.contains(containedColour) => currentMap
+          case Some(existingColours) => currentMap.updated(containedColour, outerColour :: existingColours)
+          case None => currentMap.updated(containedColour, List(outerColour))
+        }
+      })
+    }
+
+    val emptyMap: HashMap[String, List[String]] = HashMap()
+    val invertedMap: HashMap[String, List[String]] = rules.foldLeft(emptyMap)((accMap, rule) => addInvertBagMap(rule, accMap))
+    
+    val myBagColour = "shiny gold"
+
+    // Note: we need not fear cycles, as bag rules are absolute and cycles would make them un-implementable
+    def countContainers(outerColour: String, invertedMap: HashMap[String, List[String]]): Long = {
+      val emptySet: Set[String] = Set()
+      def findContainersRec(currentColour: String, acc: Set[String]): Set[String] = {
+        invertedMap.get(currentColour) match {
+          case Some(containedColours) => acc ++ containedColours.foldLeft(emptySet)((a, nextColour) => findContainersRec(nextColour, a + nextColour))
+          case None => acc
+        }
+      }
+      findContainersRec(outerColour, emptySet).size
+    }
+    println(countContainers(myBagColour, invertedMap))
+  }
+}
---
+++ 7/bagSorterStep2.scala
@@ -0,0 +1,35 @@
+import scala.io.Source
+import scala.collection.immutable.HashMap
+
+object toboganSteer {
+  def main(args: Array[String]): Unit = {
+    val filename: String        = "bagRules.txt"
+    val rules: List[String] = Source.fromFile(filename).getLines.toList
+
+    def addToMap(rule: String, initialMap: HashMap[String, List[(String, Int)]]): HashMap[String, List[(String, Int)]] = {
+      val containedRegex = "\\d+([\\w ])+[bags|bag][, |.]".r
+      val outerColour: String = rule.split(" ").slice(0, 2).mkString(" ")
+      val containedColourRules: List[String] = containedRegex.findAllIn(rule).toList
+      initialMap.updated(outerColour, containedColourRules.map(containedRule => {
+        val count: Int = containedRule.split(" ").head.toInt
+        val containedColour: String = containedRule.split(" ").slice(1, 3).mkString(" ")
+        (containedColour, count)
+      }))
+    }
+
+    val emptyMap: HashMap[String, List[(String, Int)]] = HashMap()
+    val bagsMap: HashMap[String, List[(String, Int)]] = rules.foldLeft(emptyMap)((accMap, rule) => addToMap(rule, accMap))
+    
+    val myBagColour = "shiny gold"
+
+    def countInnerBags(currentColour: String, currentBagCount: Int): Int = {
+      val innerBags = bagsMap.get(currentColour)
+      innerBags match {
+        case Some(bags) if bags.isEmpty => currentBagCount
+        case Some(bags) => currentBagCount + currentBagCount * bags.foldLeft(0)((acc, nextBag) => acc + countInnerBags(nextBag._1, nextBag._2))
+        case None => currentBagCount
+      }
+    }
+    println(countInnerBags(myBagColour, 1) - 1)
+  }
+}
